fa99b205618ccc18835ff9c419174add0b40f35b
MsPdro <119366115+MsPdro@users.noreply.github.com>
2023-01-26 18:47:02 -0300

    soft & hard skill

--- portafolio-FrontEnd/src/app/app.module.ts
+++ portafolio-FrontEnd/src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
+import { AcercaDComponent } from './components/acerca-d/acerca-d.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EstudioComponent } from './components/estudio/estudio.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialComponent
+    SocialComponent,
+    AcercaDComponent,
+    BannerComponent,
+    ExperienciaComponent,
+    EstudioComponent,
+    HabilidadesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgCircleProgressModule.forRoot({}),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ portafolio-FrontEnd/src/app/components/acerca-d/acerca-d.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acerca-d',
+  templateUrl: './acerca-d.component.html',
+  styleUrls: ['./acerca-d.component.css']
+})
+export class AcercaDComponent {
+
+}
